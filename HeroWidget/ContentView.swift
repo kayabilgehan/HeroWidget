1304541c440947796771a1db7f1eee043e994c19
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-06-02 00:03:55 +0300

    Initial Commit

---
+++ HeroWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeroWidget
+//
+//  Created by Bilgehan KAYA on 2.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
